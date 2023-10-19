25b4808c9b1fdbd9e73e26fa76e859e04bccce84
Alexander Pedro Jose Torres Apontes <alexander20012@hotmail.com>
2023-10-19 14:26:02 -0400

    cambios controlladores

--- src/controllers/rutasControllers.js
+++ src/controllers/rutasControllers.js
@@ -1,9 +1,51 @@
 const {ciudades,provincias,rutas,rutas_empresa} = require("../db");
 const { Op } = require("sequelize");
 
-exports.create = async (req,res)=>{}
-exports.update = async (req,res)=>{}
-exports.getAll = async (req,res)=>{}
-exports.getOne = async (req,res)=>{}
-exports.delete = async (req,res)=>{}
-exports.getId  = async (req,res)=>{}
+exports.create = async (req,res)=>{
+    let result="";
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+exports.update = async (req,res)=>{
+    let result="";
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+exports.getAll = async (req,res)=>{
+    let result="";
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+exports.getOne = async (req,res)=>{
+    let result="";
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+exports.delete = async (req,res)=>{
+    let result="";
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+exports.getId  = async (req,res)=>{
+    let result="";
+    try {
+        
+    } catch (error) {
+        
+    }
+}
