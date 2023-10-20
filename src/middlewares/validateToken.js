d09cf8c29132dfbca2d98204d22c7d48dbf6096a
Alexander Torres <alexander.torres@quanticoservicios.com>
2023-10-20 12:06:49 -0400

    cambio validador de token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,15 +1,14 @@
 exports.validateToken = (req, res, next) => {
-    try {
-      const token = req.headers["x-access-token"];
-      if (!token) return res.status(400).json({ message: "Prohibido el paso" });
-      const decoded = jwt.verify(token, config.secret);
-      const userFound = database.filter((u) => u.id == decoded.user[0].id);
-      if (!userFound) {
-        return res.status(403).json("Prohibido el paso");
-      }
-      req.userId = decoded.user[0].id;
-      next();
-    } catch (error) {
-      return res.status(410).json({ message: "No autorizado" });
-    }
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == null) return res.sendStatus(403);
+    jwt.verify(token, "2145ee1da63f4b794856a05c824866bf351597f0", (err, user) => {
+       if (err) return res.sendStatus(404).json({ message: "No autorizado" });
+       req.user = user;
+       next();
+    });
+  } catch (error) {
+    return res.status(410).json({ message: "No autorizado" });
+  }
 };
