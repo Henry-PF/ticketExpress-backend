44b41d82c1f6f290ad8699482524462b320c954d
Alexander Torres <alexander.torres@quanticoservicios.com>
2023-10-23 14:08:27 -0400

    cambios terminales

--- src/controllers/terminalesControllers.js
+++ src/controllers/terminalesControllers.js
@@ -1,51 +1,103 @@
-const { ciudades, provincias } = require("../db");
+const { ciudades, terminales } = require("../db");
 const { Op } = require("sequelize");
 
-exports.create = async (req, res) => {
-    let result = "";
+exports.create = async (data) => {
     try {
+        const newTerminal = await terminales.create({
+            nombre: data.nombre,
+            id_ciudad: data.id_ciudad,
+            id_statud: data.id_statud
+        })
+
+        return newTerminal;
+
+    } catch (error) {
+        return console.log({ "error": error.message });
+    }
+}
+exports.update = async (data) => {
+    try {
+        const terminal = await terminales.findOne(
+            { where: { id: { [Op.eq]: data.id } } }
+        );
+
+        if (terminal) {
+            await terminales.update({
+                nombre: data.nombre,
+                id_ciudad: data.id_ciudad,
+                id_statud: data.id_statud
+            })
+
+            return { message: "Terminal actualizado con éxito" };
+        } else {
+            return { error: "No se pudo encontrar la terminal" };
+        }
+    } catch (error) {
+        return console.log({ "error": error.message });
+    }
+}
+
+exports.getAll = async () => {
+    try {
+        const data = await terminales.findAll();
+        return data
+    } catch (error) {
+        return console.log({ "error": error.message });
+    }
+}
+exports.getOne = async (nombre) => {
+    try {
+        const terminalFound = await terminales.findOne(
+            { where: { nombre: { [Op.eq]: nombre } } }
+        );
+        if(terminalFound){
+            return terminalFound
+        } else
+
+        return "No se pudo encontrar una terminal con ese nombre"
 
     } catch (error) {
 
     }
 }
-exports.update = async (req, res) => {
-    let result = "";
+exports.deleteTerminal = async (terminalID) => {
+    let result = {};
     try {
-
+        let terminalFound = await terminales.findOne({
+            where: {
+                id: {
+                    [Op.eq]: terminalID
+                }
+            }
+        });
+        if (terminalFound) {
+            let temrinalInactive = await terminales.update({ isactivo: false }, {
+                where: {
+                    id: {
+                        [Op.eq]: terminalID
+                    }
+                }
+            });
+            if (temrinalInactive) {
+                result.data = {
+                    message: "Terminal eliminado con exito"
+                };
+            }
+        }
+        return result;
     } catch (error) {
-
+        return console.log({ "error": error.message });
     }
 }
-exports.getAll = async (req, res) => {
-    let result = "";
+exports.getId = async (id) => {
     try {
-
+        const temrinalFound = await terminales.findByPk(id)
+        if (temrinalFound) {
+            return temrinalFound
+          } else {
+           return "Terminal no encontrado";
+          }
     } catch (error) {
-
-    }
-}
-exports.getOne = async (req, res) => {
-    let result = "";
-    try {
-
-    } catch (error) {
-
-    }
-}
-exports.delete = async (req, res) => {
-    let result = "";
-    try {
-
-    } catch (error) {
-
-    }
-}
-exports.getId = async (req, res) => {
-    let result = "";
-    try {
-
-    } catch (error) {
-
+        return console.log({ "error": error.message });
     }
 }
