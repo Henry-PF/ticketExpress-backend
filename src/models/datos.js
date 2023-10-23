4441373b0b3320ccf3c4f073e457db73c149308b
Alexander Pedro Jose Torres Apontes <alexander20012@hotmail.com>
2023-10-22 22:50:54 -0400

    cambios

--- src/models/datos.js
+++ src/models/datos.js
@@ -8,31 +8,31 @@ module.exports = function (sequelize, DataTypes) {
       autoIncrement: true,
     },
     nombre: {
-      type: DataTypes.STRING(50),
+      type: DataTypes.STRING,
       allowNull: false
     },
     apellido: {
-      type: DataTypes.STRING(50),
+      type: DataTypes.STRING,
       allowNull: true
     },
     correo: {
-      type: DataTypes.STRING(50),
+      type: DataTypes.STRING,
       allowNull: true
     },
     dni: {
-      type: DataTypes.STRING(10),
+      type: DataTypes.STRING,
       allowNull: true
     },
     cuit: {
-      type: DataTypes.STRING(11),
+      type: DataTypes.STRING,
       allowNull: true
     },
     direccion: {
-      type: DataTypes.STRING(100),
+      type: DataTypes.STRING,
       allowNull: true
     },
     telefono: {
-      type: DataTypes.STRING(15),
+      type: DataTypes.STRING,
       allowNull: true
     }
   }, {
