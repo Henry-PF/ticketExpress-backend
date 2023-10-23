a0de7a3096c0d167e63edeed7c5a8fdc5750a3b6
Alexander Torres <alexander.torres@quanticoservicios.com>
2023-10-23 14:54:48 -0400

    cambios

--- src/handlers/serviciosHandler.js
+++ src/handlers/serviciosHandler.js
@@ -11,7 +11,7 @@ const handleCrearServicio = async (req, res) => {
 
 const handleServicioId = async (req, res) => {
   try {
-    await serviciosID(req, res); 
+    await serviciosID(req, res);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
