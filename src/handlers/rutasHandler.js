3ff710937030242564b9966c2709f09debfb0836
Alexander Torres <alexander.torres@quanticoservicios.com>
2023-10-23 13:52:03 -0400

    cambios

--- src/handlers/rutasHandler.js
+++ src/handlers/rutasHandler.js
@@ -4,6 +4,9 @@ exports.RutaDelete = async (req, res) => {
     let result = {};
     try {
         result = await deleteRuta(req.body.id);
+        if (result) {
+            res.status(200).json(result);
+        }
     } catch (error) {
         return res.status(500).json({ error: { message: "Error al realizar la operacion." } });
     }
@@ -13,6 +16,9 @@ exports.RutaUpdate = async (req, res) => {
     let result = {};
     try {
         result = await update(req.body.DataUpdate,req.body.id);
+        if (result) {
+            res.status(200).json(result);
+        }
     } catch (error) {
         return res.status(500).json({ error: { message: "Error al realizar la operacion." } });
     }
@@ -22,6 +28,9 @@ exports.RutaCreate = async (req, res) => {
     let result = {};
     try {
         result = await create(req.body);
+        if (result) {
+            res.status(200).json(result);
+        }
     } catch (error) {
         return res.status(500).json({ error: { message: "Error al realizar la operacion." } });
     }
@@ -31,6 +40,9 @@ exports.RutagetAll = async (req, res) => {
     let result = {};
     try {
         result = await getAll();
+        if (result) {
+            res.status(200).json(result);
+        }
     } catch (error) {
         return res.status(500).json({ error: { message: "Error al realizar la operacion." } });
     }
@@ -40,6 +52,9 @@ exports.RutagetId = async (req, res) => {
     let result = {};
     try {
         result = await getId(req.body.id);
+        if (result) {
+            res.status(200).json(result);
+        }
     } catch (error) {
         return res.status(500).json({ error: { message: "Error al realizar la operacion." } });
     }
@@ -49,6 +64,9 @@ exports.RutagetOne = async (req, res) => {
     let result = {};
     try {
         result = await getOne(req.body.where);
+        if (result) {
+            res.status(200).json(result);
+        }
     } catch (error) {
         return res.status(500).json({ error: { message: "Error al realizar la operacion." } });
     }
